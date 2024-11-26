aea5c63455296dfc1ef2d15d276ea0ef3a972cec
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-11-26 21:31:37 +0200

    Enhanced styling and functionality

--- main.js
+++ main.js
@@ -7,110 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/contact.js":
-/*!************************!*\
-  !*** ./src/contact.js ***!
-  \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ loadContact)\n/* harmony export */ });\nfunction loadContact() {\n    const contact = document.createElement('div');\n    contact.innerHTML = `\n      <h1>Contact Us</h1>\n      <p>Email: contact@restaurant.com</p>\n      <p>Phone: +123-456-7890</p>\n    `;\n    return contact;\n  }\n  \n\n//# sourceURL=webpack://restaurant-page/./src/contact.js?");
-
-/***/ }),
-
-/***/ "./src/home.js":
-/*!*********************!*\
-  !*** ./src/home.js ***!
-  \*********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ loadHome)\n/* harmony export */ });\nfunction loadHome() {\n    const home = document.createElement('div');\n    home.innerHTML = `\n      <h1>Welcome to Our Restaurant</h1>\n      <p>Enjoy delicious food and great ambiance!</p>\n    `;\n    return home;\n  }\n  \n\n//# sourceURL=webpack://restaurant-page/./src/home.js?");
-
-/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menu */ \"./src/menu.js\");\n/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./contact */ \"./src/contact.js\");\n\n\n\n\nfunction loadPage(pageFunction) {\n  const content = document.getElementById('content');\n  content.innerHTML = ''; \n  content.appendChild(pageFunction()); \n}\n\n\ndocument.addEventListener('DOMContentLoaded', () => {\n  const navButtons = document.querySelectorAll('nav button');\n  navButtons[0].addEventListener('click', () => loadPage(_home__WEBPACK_IMPORTED_MODULE_0__[\"default\"]));\n  navButtons[1].addEventListener('click', () => loadPage(_menu__WEBPACK_IMPORTED_MODULE_1__[\"default\"]));\n  navButtons[2].addEventListener('click', () => loadPage(_contact__WEBPACK_IMPORTED_MODULE_2__[\"default\"]));\n\n  loadPage(_home__WEBPACK_IMPORTED_MODULE_0__[\"default\"]); //by default\n});\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/menu.js":
-/*!*********************!*\
-  !*** ./src/menu.js ***!
-  \*********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ loadMenu)\n/* harmony export */ });\nfunction loadMenu() {\n    const menu = document.createElement('div');\n    menu.innerHTML = `\n      <h1>Our Menu</h1>\n      <ul>\n        <li>Pizza - $10</li>\n        <li>Pasta - $12</li>\n        <li>Salad - $8</li>\n      </ul>\n    `;\n    return menu;\n  }\n  \n\n//# sourceURL=webpack://restaurant-page/./src/menu.js?");
+eval("document.getElementById('home').addEventListener('click', () => {\n  document.getElementById('content').innerHTML = `\n      <div>\n          <h1>Welcome to Our Restaurant</h1>\n          <p>Enjoy delicious food and great ambiance!</p>\n          <img src=\"https://via.placeholder.com/600x400\" alt=\"Restaurant\" style=\"width: 80%; border-radius: 10px;\">\n      </div>\n  `;\n});\n\ndocument.getElementById('menu').addEventListener('click', () => {\n  document.getElementById('content').innerHTML = `\n      <div>\n          <h1>Our Menu</h1>\n          <p>Delicious dishes crafted with love and care.</p>\n          <ul>\n              <li>Pizza</li>\n              <li>Pasta</li>\n              <li>Salad</li>\n              <li>Desserts</li>\n          </ul>\n      </div>\n  `;\n});\n\ndocument.getElementById('contact').addEventListener('click', () => {\n  document.getElementById('content').innerHTML = `\n      <div>\n          <h1>Contact Us</h1>\n          <p>Email: contact@ourrestaurant.com</p>\n          <p>Phone: +123 456 7890</p>\n      </div>\n  `;\n});\n\n// Load Home page by default\ndocument.getElementById('home').click();\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
 /******/ })()
 ;
