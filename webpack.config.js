638700166f4d52f7b6ab0faa44189f21c52dff7b
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-11-26 21:53:15 +0200

    Updated design

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,45 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  
+  // Entry point for the application
   entry: './src/index.js',
   output: {
-   
-    filename: 'main.js', 
-    path: path.resolve(__dirname, 'dist'), 
-    clean: true, 
+    filename: 'main.js', // Output JavaScript file name
+    path: path.resolve(__dirname, 'dist'), // Output directory
   },
-  mode: 'development', 
-  devServer: {
-   
-    static: './dist', 
-    port: 5500, 
-    open: true, 
-  },
+  mode: 'development', // Change to 'production' for production build
   module: {
-   
     rules: [
       {
-        
-        test: /\.css$/i, 
-        use: ['style-loader', 'css-loader'], 
+        // Rule for JavaScript/ES6 files
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], // Transpile ES6+ code
+          },
+        },
+      },
+      {
+        // Rule for CSS files
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // Loads and injects CSS
+      },
+      {
+        // Rule for image files (if you add images later)
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]', // Output path for images
+        },
       },
     ],
   },
-  plugins: [
-    
-    new HtmlWebpackPlugin({
-      template: './src/template.html', 
-    }),
-  ],
+  devtool: 'eval-source-map', // Source maps for easier debugging
+  devServer: {
+    static: './dist', // Serve files from the dist directory
+    hot: true, // Enable hot module replacement
+  },
+  plugins: [],
 };
-
