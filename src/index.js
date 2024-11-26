89123e179dbfe058835280174d4d489a6ec26394
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-11-26 20:28:03 +0200

    message

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import loadContact from './contact';
 
 function loadPage(pageFunction) {
   const content = document.getElementById('content');
-  content.innerHTML = ''; // Clear the current content
-  content.appendChild(pageFunction()); // Load new content
+  content.innerHTML = ''; 
+  content.appendChild(pageFunction()); 
 }
 
-// Event listeners for navigation
+
 document.addEventListener('DOMContentLoaded', () => {
   const navButtons = document.querySelectorAll('nav button');
   navButtons[0].addEventListener('click', () => loadPage(loadHome));
   navButtons[1].addEventListener('click', () => loadPage(loadMenu));
   navButtons[2].addEventListener('click', () => loadPage(loadContact));
 
-  loadPage(loadHome); // Load Home page by default
+  loadPage(loadHome); //by default
 });
