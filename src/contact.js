7c76743edd44f81ed4d1e80be720c58c1bb9f216
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-11-29 22:22:50 +0200

    Updated CSS and design

--- src/contact.js
+++ src/contact.js
@@ -3,7 +3,7 @@ export default function loadContact() {
     contact.innerHTML = `
       <h1>Contacts</h1>
       <p>Email: 230108@astanait.edu.kz</p>
-      <p>Phone: +370 663 62 256/p>
+      <p>Phone: +370 663 62 256 </p>
     `;
     return contact;
   }
